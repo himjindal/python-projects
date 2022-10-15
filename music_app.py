0066324ae04df7fcc2c4c3f31380cb760e4ce956
himjindal <103064653+himjindal@users.noreply.github.com>
2022-10-14 23:03:37 -0700

    Update music_app.py

--- music_app.py
+++ music_app.py
@@ -19,8 +19,13 @@ elif choice == "Dataset-Upload Sample Data":
     df_sample = pd.DataFrame(read_data_sample)
     df_x = df_sample.drop(columns = ["target"])
     df_y = df_sample["target"]
+    
+    st.write("X_Sample")
     data_x = st.dataframe(df_x)
+    
+    st.write("Y_Sample")
     data_y = st.dataframe(df_y)
+    
     st.write("Congrats File Uploaded")
     
     
@@ -29,6 +34,8 @@ elif choice == "Dataset-Upload Test Data":
   data_uploaded_test = st.file_uploader("Please upload test csv",type=["csv"])
   if data_uploaded_test is not None:
     read_data_test = pd.read_csv(data_uploaded_test)
+    
+    st.write("X_Test")
     df_test = st.dataframe(read_data_test)
+    
     st.write("Congrats File Uploaded")
-    st.write("Shape of your dataset: ",df_test.shape)
