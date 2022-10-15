f9d2f16b7e25e96c323f769cbc05804175d0bf99
himjindal <103064653+himjindal@users.noreply.github.com>
2022-10-14 21:16:57 -0700

    Update music_app.py

--- music_app.py
+++ music_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from IPython.display import display
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+import re
+import random
+from sklearn import preprocessing
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+
+import warnings
+warnings.filterwarnings('ignore')
 from sklearn.ensemble import RandomForestClassifier
 model = RandomForestClassifier()
 
