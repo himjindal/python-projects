4658bc4e6c5b2de223b41ec76789d54671662d21
himjindal <103064653+himjindal@users.noreply.github.com>
2022-10-14 21:04:18 -0700

    Update music_app.py

--- music_app.py
+++ music_app.py
@@ -1,12 +1,3 @@
 import streamlit as st
 
 st.title ("Music Recommendation Project")
-
-menu = ["Home","Dataset"]
-
-choice = st.sidebar.selections("Menu",menu)
-
-if choice == "Home":
-    st.subheader("Home")
-elif choice == "Dataset":
-    st.subheader("Dataset")
