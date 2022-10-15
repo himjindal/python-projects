332342da318b546fd86e9e647ea3240a0e5a049a
himjindal <103064653+himjindal@users.noreply.github.com>
2022-10-14 21:53:29 -0700

    Update music_app.py

--- music_app.py
+++ music_app.py
@@ -10,13 +10,17 @@ choice = st.sidebar.selectbox("Menu",menu)
 
 if choice == "Home":
   st.subheader("Home")
+  
 elif choice == "Dataset-Upload Sample Data":
   st.subheader("Dataset-Upload Sample Data")
   data_uploaded_sample = st.file_uploader("Please upload sample csv",type=["csv"])
-  read_data_sample = pd.read_csv(data_uploaded_sample)
-  df_sample = st.dataframe(read_data_sample)
+  if data_uploaded_sample is not none:
+    read_data_sample = pd.read_csv(data_uploaded_sample)
+    df_sample = st.dataframe(read_data_sample)
+    
 elif choice == "Dataset-Upload Test Data":
   st.subheader("Dataset-Upload Test Data")
-  data_uploaded_test = st.file_uploader("Please upload sample csv",type=["csv"])
-  read_data_test = pd.read_csv(data_uploaded_test)
-  df_test = st.dataframe(read_data_test)
+  data_uploaded_test = st.file_uploader("Please upload test csv",type=["csv"])
+  if data_uploaded_test is not none:
+    read_data_test = pd.read_csv(data_uploaded_test)
+    df_test = st.dataframe(read_data_test)
